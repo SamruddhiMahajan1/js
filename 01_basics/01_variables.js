3b47f187787a741cd7a40c222a7106deca96c5aa
Samruddhi Mahajan <samruddhimahajan123@gmail.com>
2026-06-20 14:28:23 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=1234
+let accountEmail="samruddhimahajan123@gmail.com"
+var accountPassword="212121"
+accountCity="Jaipur"
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
