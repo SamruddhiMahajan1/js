bda86fca11bc81774f461d7fdd6830b1f0db04a3
Samruddhi Mahajan <samruddhimahajan123@gmail.com>
2026-06-21 11:48:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score="33abc"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/* "33"=>33
+"33abs"=>NaN
+true=1; false =0
+*/
+let isLoggedIn=1
+let booleanIsloggedIn=Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+//1 = true, 0=flase
+//""=false
+//"hitesh"=>true
+
+let some=33
+let stringNumber=String(some)
+console.log(typeof stringNumber); //string
